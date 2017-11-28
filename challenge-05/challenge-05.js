1628598fb47ab73c03e7766aae31770e0c121309
Cesar Lima <cesarcamposdelima@gmail.com>
2017-11-28 21:09:51 -0200

    Challenge-05

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var myArray = [1, true, 'JavaScript', null, undefined];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function myFunction(arr) {
+  return arr;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+console.log(myFunction(myArray)[1]);
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +22,25 @@ segundo, um número. A função deve retornar um índice do array que foi passad
 no primeiro parâmetro. O índice a ser retornado, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function functionTwoParameters(array, num) {
+  return array[num];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var arrayFiveTypes = ['Natal', false, NaN, 60,{x:';D'}];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+console.log(functionTwoParameters(arrayFiveTypes, 0));
+console.log(functionTwoParameters(arrayFiveTypes, 1));
+console.log(functionTwoParameters(arrayFiveTypes, 2));
+console.log(functionTwoParameters(arrayFiveTypes, 3));
+console.log(functionTwoParameters(arrayFiveTypes, 4));
+
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +56,51 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book(bookName){
+  var books = {
+    'O Peregrino': {
+      quantidadePaginas: 300,
+      autor: 'John Bunyan',
+      editora: 'Publicações RBC'
+    },
+    'Use a Cabeça Padrões de Projetos': {
+      quantidadePaginas: 500,
+      autor: 'Elisabeth Robson, Kathy Sierra',
+      editora: 'Alta Books'
+    },
+    'Implementando Domain Driven Design': {
+      quantidadePaginas: 591,
+      autor: 'Vaughn Vernon',
+      editora: 'Alta Books'
+    },
+  };
+
+  return books[bookName] ? books[bookName] : books;
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+console.log(book());
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+var bookName = 'O Peregrino';
+console.log('O livro '+ bookName + ' tem ' + book(bookName).quantidadePaginas + ' páginas!');
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+console.log('O autor do livro ' + bookName + ' é ' + book(bookName).autor + '.');
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+console.log('O livro ' + bookName + ' foi publicado pela editora ' + book(bookName).editora + '.');
